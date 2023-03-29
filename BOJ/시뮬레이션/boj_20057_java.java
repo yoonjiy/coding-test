4919bdaaa4fcf98ed6bc1d61b6b05807b19fce21
YOONJI KIM <stritegdc@gmail.com>
2023-03-29 23:50:25 +0900

    [boj]: 20058 마법사 상어와 파이어스톰 - 시뮬레이션, bfs, 배열 회전

--- BOJ/시뮬레이션/boj_20057_java.java
+++ BOJ/시뮬레이션/boj_20057_java.java
@@ -6,19 +6,20 @@ import java.io.*;
 public class boj_20057_java {
     static int[] dr = {0, 1, 0, -1}; //서, 남, 동, 북
     static int[] dc = {-1, 0, 1, 0};
-    static int spreadR[][] = {
-        {-1, 1, -1, 1, -1, 1, -2, 2, 0, 0},
-        {0, 0, 1, 1, 2, 2, 1, 1, 3, 2},
-        {-1, 1, -1, 1, -1, 1, -2, 2, 0, 0},
-        {0, 0, -1, -1, -2, -2, -1, -1, -3, -2}
+    //1 1 2 2 7 7 10 10 5 a
+    static int[][] spreadR = {
+        {-1, 1, -2, 2, -1, 1, -1, 1, 0, 0},
+        {0, 0, 1, 1, 1, 1, 2, 2, 3, 2},
+        {-1, 1, -2, 2, -1, 1, -1, 1, 0, 0},
+        {0, 0, -1, -1, -1, -1, -2, -2, -3, -2}
     };
-    static int spreadC[][] = {
-        {0, 0, -1, -1, -2, -2, -1, -1, -3, -2},
-        {-1, 1, -1, 1, -1, 1, -2, 2, 0, 0},
-        {0, 0, 1, 1, 2, 2, 1, 1, 3, 2},
-        {-1, 1, -1, 1, -1, 1, -2, 2, 0, 0}
+    static int[][] spreadC = {
+        {0, 0, -1, -1, -1, -1, -2, -2, -3, -2},
+        {-1, 1, -2, 2, -1, 1, -1, 1, 0, 0},
+        {0, 0, 1, 1, 1, 1, 2, 2, 3, 2},
+        {-1, 1, -2, 2, -1, 1, -1, 1, 0, 0}
     };
-    static int percent[] = { 1, 1, 7, 7, 10, 10, 2, 2, 5 };
+    static int[] percent = {1, 1, 2, 2, 7, 7, 10, 10, 5};
     
     static int[][] sand;
     static int answer;
@@ -36,27 +37,25 @@ public class boj_20057_java {
             }
         }
         
-        int r, c;
-        r = n/2;
-        c = n/2;
         
         int moveCnt = 1;
-        int dir = 0;
-
+        int r = n/2;
+        int c = n/2;
+        int dir = 0; //서, 남, 동, 북으로 이동
         while(true){
-            for(int i=0; i<2; i++){ //moveCnt;
+            for(int i=0; i<2; i++){
                 for(int j=0; j<moveCnt; j++){
                     spreadSand(r, c, dir);
                     r += dr[dir];
                     c += dc[dir];
                 }
-                //방향 전환
-                dir = (dir+1)%4;
+                dir = (dir+1)%4; 
             }
-
+            
             moveCnt++;
-    
+
             if(moveCnt==n){
+                //마지막 이동
                 for(int i=0; i<moveCnt-1; i++){
                     spreadSand(r, c, dir);
                     r += dr[dir];
@@ -67,40 +66,45 @@ public class boj_20057_java {
         }
 
         System.out.println(answer);
+
     }
     
-    private static void spreadSand(int r, int c, int dir){
+    public static void spreadSand(int r, int c, int dir){
         int nr = r + dr[dir];
-        int nc = c + dc[dir]; 
-        int curr = sand[nr][nc];
-        
+        int nc = c + dc[dir];
+        int s = sand[nr][nc];
+
         for(int i=0; i<9; i++){
+            //모래 흩날림
             int nnr = r + spreadR[dir][i];
             int nnc = c + spreadC[dir][i];
-            
-            int plus = curr*percent[i]/100;
-            
-            if(nnr<0 || nnc<0 || nnr>=n || nnc>=n){
-                //격자 밖으로 나간 모래
-                answer += plus;
+
+            int add = s*percent[i]/100;
+
+            if(nnr<0 || nnc<0 || nnr>=n || nnc>=n) {
+                //범위를 벗어나는 모래 
+                answer += add;
             }
             else{
-                sand[nnr][nnc] += plus;
-                
+                sand[nnr][nnc] += add;
             }
-            sand[nr][nc] -= plus;
+
+            sand[nr][nc] -= add;
         }
-        
-        //a에 모래 이동
-        int ar = r + spreadR[dir][9];
-        int ac = c + spreadC[dir][9];
-        if(ar<0 || ac<0 || ar>=n || ac>=n ){
+
+        //a칸으로 모래 이동
+        int nnr = r + spreadR[dir][9];
+        int nnc = c + spreadC[dir][9];
+
+        if(nnr<0 || nnc<0 || nnr>=n || nnc>=n) {
+            //범위를 벗어나는 모래 
             answer += sand[nr][nc];
         }
         else{
-            sand[ar][ac] += sand[nr][nc];
+            sand[nnr][nnc] += sand[nr][nc];
         }
+
         sand[nr][nc] = 0;
-        
+
     }
 }
---
+++ BOJ/시뮬레이션/boj_20058_java.java
@@ -0,0 +1,160 @@
+package 시뮬레이션;
+
+import java.util.*;
+import java.io.*;
+
+public class boj_20058_java{
+    static int n, q;
+    static int[][] board;
+    static int[] order;
+    static int len;
+    static int[] dr = {0, 0, -1, 1};
+    static int[] dc = {-1, 1, 0, 0};
+    static boolean[][] visited;
+    static int sum, count;
+    public static class Point{
+        int r, c;
+        Point(int r, int c){
+            this.r = r;
+            this.c = c;
+        }
+    }
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        q = Integer.parseInt(st.nextToken());
+
+        len = (int)Math.pow(2, n);//2^n
+        board = new int[len][len];
+        for(int i=0; i<len; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<len; j++){
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        order = new int[q];
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<q; i++){
+            //마법사 상어가 시전한 단계
+            order[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<q; i++){
+            //격자를 나누고 시계 방향 90도 회전시킨다
+            board = divide(order[i]);
+            //얼음이 있는 칸 3개 이상과 인접해있지 않은 칸은 얼음양이 1 줄어든다
+            board = melt();
+        }
+
+        //남아있는 얼음의 합, 가장 큰 덩어리가 차지하는 칸의 개수
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+                if(board[i][j]==0) continue;
+                sum += board[i][j];
+            }
+        }
+
+        visited = new boolean[len][len];
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+                if(!visited[i][j] && board[i][j]!=0) {
+                    count = Math.max(count, bfs(i, j));
+            
+                }
+            }
+        }
+
+        System.out.println(sum);
+        System.out.println(count);
+        
+    }
+
+    public static int bfs(int r, int c){
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(r, c));
+        visited[r][c] = true;
+
+        int cnt = 1; //칸의 개수
+
+        while(!q.isEmpty()){
+            Point p = q.poll();
+
+            for(int i=0; i<4; i++){
+                int nr = p.r + dr[i];
+                int nc = p.c + dc[i];
+
+                if(nr<0 || nc<0 || nr>=len || nc>=len || visited[nr][nc] || board[nr][nc]==0) continue;
+
+                visited[nr][nc] = true;
+                cnt++;
+                q.add(new Point(nr, nc));
+            }
+        }
+
+        return cnt;
+    }
+
+    public static int[][] divide(int l){
+        int[][] temp = new int[len][len];
+
+        l = (int)Math.pow(2, l);
+
+        //배열 시계 방향 90도 회전
+        for(int i=0; i<len; i+=l){
+            for(int j=0; j<len; j+=l){
+                rotate(i, j, l, temp);
+            }
+        }
+
+        return temp;
+    }
+
+    public static void rotate(int r, int c, int w, int[][] temp){
+        // for(int i=r; i<r+w; i++){
+        //     for(int j=c; j<c+w; j++){ 
+        //         temp[i][j] = board[r+w-j+c-1][i];
+        //     }
+        // }
+        for (int i=0; i<w; i++) {
+            for (int j=0; j<w; j++) {
+                //temp[r+j][c+w-i-1] = board[r+i][c+j];
+                temp[r + i][c + j] = board[r + w - 1 - j][c + i];
+            }
+        }
+    }
+
+    //얼음은 동시에 녹음
+    public static int[][] melt(){
+
+        int[][] temp = new int[len][len];
+        for (int i=0; i<len; i++) 
+            temp[i] = Arrays.copyOf(board[i], len); //얼음이 녹은 걸 반영해줄 배열
+
+        for(int i=0; i<len; i++){
+            for(int j=0; j<len; j++){
+
+                if(board[i][j]==0) continue;
+
+                int cnt = 0;
+                for(int k=0; k<4; k++){
+                    int nr = i + dr[k];
+                    int nc = j + dc[k];
+
+                    if(nr<0 || nc<0 || nr>=len || nc>=len || board[nr][nc]==0) continue;
+
+                    cnt++;
+                }
+
+                if(cnt<=2){
+                    //얼음이 녹음
+                    temp[i][j]--;
+                }
+            }
+        }
+
+        return temp;
+    }
+        
+}
